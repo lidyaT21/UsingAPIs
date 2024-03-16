146dab9dba8f023082887a0f55286a086b36a33d
Lidya Tesfamariam <zenilisa1221@gmail.com>
2024-03-16 13:52:47 +0300

    prep-exercise

--- Week1/prep-exercises/1-catwalk-promises/index.js
+++ Week1/prep-exercises/1-catwalk-promises/index.js
@@ -8,20 +8,27 @@ const DANCING_CAT_URL =
 
 function walk(img, startPos, stopPos) {
   return new Promise((resolve) => {
-    // Resolve this promise when the cat (`img`) has walked from `startPos` to
-    // `stopPos`.
-    // Make good use of the `STEP_INTERVAL_PX` and `STEP_INTERVAL_MS`
-    // constants.
+   const intervalId =setInterval(function(){
+    img.style.left= startPos+'px';
+    startPos+= STEP_SIZE_PX;
+    if(startPos>stopPos){
+      clearInterval(intervalId);
+      resolve()
+    }
+  },STEP_INTERVAL_MS)
   });
 }
 
 function dance(img) {
   return new Promise((resolve) => {
-    // Switch the `.src` of the `img` from the walking cat to the dancing cat
-    // and, after a timeout, reset the `img` back to the walking cat. Then
-    // resolve the promise.
-    // Make good use of the `DANCING_CAT_URL` and `DANCE_TIME_MS` constants.
-  });
+   img.src = DANCING_CAT_URL;
+    setTimeout(()=>{
+      img.src = "http://www.anniemation.com/clip_art/images/cat-walk.gif";
+       resolve();
+  
+    },DANCE_TIME_MS);
+   });
+
 }
 
 function catWalk() {
@@ -35,6 +42,10 @@ function catWalk() {
   // 2. Then dance for 5 secs.
   // 3. Then walk from `centerPos` to `stopPos`.
   // 4. Repeat the first three steps indefinitely.
+  walk(img,startPos,centerPos)
+  .then(()=>dance(img))
+  .then(()=>walk(img,centerPos,stopPos))
+  .then(()=>catWalk());
 }
 
 window.addEventListener('load', catWalk);
